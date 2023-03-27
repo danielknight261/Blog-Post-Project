01915e9601f38d391fd67cdbe979761ebae182da
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-03-27 15:50:41 +0100

    login.js file complete

--- client/src/pages/auth/login.js
+++ client/src/pages/auth/login.js
@@ -2,17 +2,32 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const login = () => {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+
   // Sign in with google auth
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
+      route.push("/");
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    if (user) {
+      route.push("/");
+    } else {
+      console.log("login");
+    }
+  }, [user]);
 
   return (
     <div className="shadow-xl mx-10 mt-32 p-10 text-gray-700">
