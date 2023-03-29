3a15fd31f875167cf5cb177d644e2f7a710e8353
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-03-29 10:32:51 +0100

    post page creatred and state added

--- client/components/layout.js
+++ client/components/layout.js
@@ -5,8 +5,8 @@ export default function Layout({ children }) {
   return (
     // Apply margin and font family to the entire layout
     <div className="mx-6 md:max-w-2xl md:mx-auto font-poppins">
-      <Nav /> // Include the navigation bar component
-      <main>{children}</main> // Render the main content passed as props
+      <Nav /> 
+      <main>{children}</main>
     </div>
   );
 }
--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,20 +1,41 @@
-import { Auth } from "../../utils/firebase"
-import { useAuthState } from "react-firebase-hooks/auth"
-import { useRouter } from "next/router"
-import {useEffect, useState} from "react"
+import { Auth } from "../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export default function Post(){
-    return(
-        <div className="m-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-            <form>
-                <h1 className="text-2xl font-bold">Create a New Post</h1>
-                <div className="py-2">
-                    <h3 className="tex-lg front-medium py-2">Description</h3>
-                    <textarea className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-small"></textarea>
-                    <p className="">/300</p>
-                </div>
-                <button>Submit</button>
-            </form>
+export default function Post() {
+  //Form State
+  const [post, setPost] = useState({ description: "" });
+
+  //submit post
+  const submitPost = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="m-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
+      <form onSubmit={submitPost}>
+        <h1 className="text-2xl font-bold">Create a New Post</h1>
+        <div className="py-2">
+          <h3 className="tex-lg front-medium py-2">Description</h3>
+          <textarea
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-small"
+          ></textarea>
+          <p className={`text-cyan-600 font-medium text-sm ${
+              post.description.length > 300 ? "text-red-600" : ""
+            }`}>
+            {post.description.length}/300
+          </p>
         </div>
-    )
+        <button
+          type="submit"
+          className="w-full bg-cyan-600  text-white font-medium p-2 y-2 rounded-lg text-sm"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
