bd0cdbfa061f3200c788e5808808b7f314275484
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-04-03 12:22:58 +0100

    post home page card

--- client/components/message.js
+++ client/components/message.js
@@ -3,7 +3,7 @@ import Head from "next/head"
 export default function Message({ children }) {
   return (
     <div className="bg-white p-8 border-b-2 rounded-lg">
-      <div>
+      <div className="flex items-center">
         <img src="" />
         <h2>user</h2>
       </div>
